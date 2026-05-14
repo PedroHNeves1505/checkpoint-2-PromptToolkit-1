56d62310f02d36f39698ac4b21fde6aa59764d79
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-05-14 19:28:51 -0300

    fix: __init__

--- checkpoint-2-PromptToolkit/src/__init__.py
+++ checkpoint-2-PromptToolkit/src/__init__.py
@@ -1,15 +1,9 @@
-from .evaluator import Evaluator
 from .llm_client import LLMClient
-from .prompt_builder import PromptBuilder
-from .report import Report
-from .tasks import Tasks
-from .techniques import Techniques
+from .prompt_builder import montar_prompt, adicionar_exemplos, adicionar_cot
 
 __all__ = [
-    "Evaluator",
     "LLMClient",
-    "PromptBuilder",
-    "Report",
-    "Tasks",
-    "Techniques",
+    "montar_prompt",
+    "adicionar_exemplos",
+    "adicionar_cot",
 ]
